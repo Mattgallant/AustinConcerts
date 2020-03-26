c5130918d91eb5e7a266139792ee6242d190bdf0
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-03-26 10:12:46 -0500

    Got splash page working with Django Server. Now has images and custom CSS.

--- DjangoApp/austin_data_bass/webapp/views.py
+++ DjangoApp/austin_data_bass/webapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'webapp/home.html')
+    return render(request, 'webapp/index.html')
 
 
 def about(request):
