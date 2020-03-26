5a7cd8a6f725abeb9c9463e7b774ce94c2f5b614
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-03-26 10:35:00 -0500

    Set up links to all model home pages, reformatted splash page to use navbar template

--- DjangoApp/austin_data_bass/webapp/urls.py
+++ DjangoApp/austin_data_bass/webapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='webapp-home'),
-    path('about/', views.about, name='webapp-about')
+    path('about/', views.about, name='webapp-about'),
+    path('concerts/', views.concerts, name='webapp-concerts'),
+    path('venues/', views.venues, name='webapp-venues'),
+    path('artists/', views.artists, name='webapp-artists'),
 ]
--- DjangoApp/austin_data_bass/webapp/views.py
+++ DjangoApp/austin_data_bass/webapp/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. These are called from urls.py.
+# A URL will essentially request a certain "view". Process
+# and display that view here. 
 def home(request):
     return render(request, 'webapp/index.html')
 
-
 def about(request):
+    return render(request, 'webapp/about.html')
+
+def concerts(request):
     return HttpResponse('<h1>About</h1>')
+
+def artists(request):
+    return HttpResponse('<h1>About</h1>')
+
+def venues(request):
+    return HttpResponse('<h1>About</h1>')    
