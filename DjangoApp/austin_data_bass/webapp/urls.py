1436df69b3b0ab4ee533d5dfb2a409ae24400ad5
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-03-29 17:01:58 -0500

    Linked up the concerts grid page, create concerts instance pages, updated the admin page to include concerts model

--- DjangoApp/austin_data_bass/webapp/urls.py
+++ DjangoApp/austin_data_bass/webapp/urls.py
@@ -2,15 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	#Home and about pages
     path('', views.home, name='webapp-home'),
     path('about/', views.about, name='webapp-about'),
+
+    #Models
     path('concerts/', views.concerts, name='webapp-concerts'),
+    path('concerts/<str:concert_name>/', views.concert_name, name='webapp-concerts-instance'),
     path('venues/', views.venues, name='webapp-venues'),
     path('venues/<str:venue_name>/', views.venue_name, name='webapp-venues-instance'),
-
     path('artists/', views.artists, name='webapp-artists'),
     path('artists/<str:artist_name>/', views.artist_name, name='webapp-artists-instance'),
-#    path('concerts/<str:concert_name>/', views.concert_name, name='webapp-concerts-instance'),
+
     #Development url... just for messing around
     path('dev/', views.dev, name='webapp-dev'),
 ]
