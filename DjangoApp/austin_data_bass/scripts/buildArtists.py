2ee03efd4d66a5ff9068185cfb2948cd95f7bace
Will Worthington <willworthington99@yahoo.com>
2020-03-29 16:22:00 -0500

    made script to automate building artists in the shell

---
+++ DjangoApp/austin_data_bass/scripts/buildArtists.py
@@ -0,0 +1,12 @@
+from webapp.models import Artist, Concerts
+
+artistlist = Artist.objects.all()
+for artist in artistlist:
+    artist.delete()
+    
+concertlist = Concerts.objects.all()
+for c in concertlist:
+    artist = Artist.create(c.artists[0], c.concertName)
+    if artist is not None:
+        artist.save()
+        
